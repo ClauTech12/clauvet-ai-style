5cccc16089c5cd8a05c8a747e5a3758dbcb96c73
Agbor Clauvet <clauvetmt19988@gmail.com>
2026-09-01 06:50:54 +0530

    Enable CSRF middleware for server functions

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,4 @@
-import { createStart, createMiddleware } from "@tanstack/react-start";
+import { createStart, createMiddleware, createCsrfMiddleware } from "@tanstack/react-start";
 
 import { renderErrorPage } from "./lib/error-page";
 import { captureServerException } from "./lib/sentry-server";
@@ -19,6 +19,10 @@ const errorMiddleware = createMiddleware().server(async ({ next }) => {
   }
 });
 
+const csrfMiddleware = createCsrfMiddleware({
+  filter: (ctx) => ctx.handlerType === "serverFn",
+});
+
 export const startInstance = createStart(() => ({
-  requestMiddleware: [errorMiddleware],
+  requestMiddleware: [csrfMiddleware, errorMiddleware],
 }));
